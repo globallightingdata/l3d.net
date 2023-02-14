809638a5c3d594a3c1408bca88e738d3d77a5086
Kurpanik <kurpanik@dial.de>
2023-02-14 16:15:59 +0100

    Refactoring reading container from bytes

--- src/L3D.Net/Internal/ContainerReader.cs
+++ src/L3D.Net/Internal/ContainerReader.cs
@@ -1,46 +1,44 @@
-﻿using System;
-using System.IO;
-using L3D.Net.Abstract;
+﻿using L3D.Net.Abstract;
 using L3D.Net.API.Dto;
 using L3D.Net.Internal.Abstract;
+using System;
+using System.IO;
 
-namespace L3D.Net.Internal
+namespace L3D.Net.Internal;
+
+internal class ContainerReader : IContainerReader
 {
-    internal class ContainerReader : IContainerReader
+    private readonly IFileHandler _fileHandler;
+    private readonly IL3dXmlReader _l3dXmlReader;
+    private readonly IApiDtoConverter _apiDtoConverter;
+
+    public ContainerReader(IFileHandler fileHandler, IL3dXmlReader l3dXmlReader, IApiDtoConverter apiDtoConverter)
     {
-        private readonly IFileHandler _fileHandler;
-        private readonly IL3dXmlReader _l3dXmlReader;
-        private readonly IApiDtoConverter _apiDtoConverter;
+        _fileHandler = fileHandler ?? throw new ArgumentNullException(nameof(fileHandler));
+        _l3dXmlReader = l3dXmlReader ?? throw new ArgumentNullException(nameof(l3dXmlReader));
+        _apiDtoConverter = apiDtoConverter ?? throw new ArgumentNullException(nameof(apiDtoConverter));
+    }
 
-        public ContainerReader(IFileHandler fileHandler, IL3dXmlReader l3dXmlReader, IApiDtoConverter apiDtoConverter)
-        {
-            _fileHandler = fileHandler ?? throw new ArgumentNullException(nameof(fileHandler));
-            _l3dXmlReader = l3dXmlReader ?? throw new ArgumentNullException(nameof(l3dXmlReader));
-            _apiDtoConverter = apiDtoConverter ?? throw new ArgumentNullException(nameof(apiDtoConverter));
-        }
+    public LuminaireDto Read(string containerPath)
+    {
+        if (string.IsNullOrWhiteSpace(containerPath))
+            throw new ArgumentException(@"Value cannot be null or whitespace", nameof(containerPath));
+        return ReadInternal(directory => _fileHandler.ExtractContainerToDirectory(containerPath, directory));
+    }
 
-        public LuminaireDto Read(string containerPath)
-        {
-            if (string.IsNullOrWhiteSpace(containerPath))
-                throw new ArgumentException(@"Value cannot be null or whitespace.", nameof(containerPath));
+    public LuminaireDto Read(byte[] containerBytes)
+    {
+        if (containerBytes == null || containerBytes.LongLength == 0)
+            throw new ArgumentException(@"Value cannot be null or empty array", nameof(containerBytes));
+        return ReadInternal(directory => _fileHandler.ExtractContainerToDirectory(containerBytes, directory));
+    }
 
-            using var directoryScope = new ContainerDirectoryScope(_fileHandler.CreateContainerDirectory());
-            _fileHandler.ExtractContainerToDirectory(containerPath, directoryScope.Directory);
-            var structurePath = Path.Combine(directoryScope.Directory, Constants.L3dXmlFilename);
-            var luminaire = _l3dXmlReader.Read(structurePath, directoryScope.Directory);
-            return _apiDtoConverter.Convert(luminaire, directoryScope.Directory);
-        }
-
-        public LuminaireDto Read(byte[] containerBytes)
-        {
-            if (containerBytes == null || containerBytes.LongLength == 0)
-                throw new ArgumentException(@"Value cannot be null or empty array.", nameof(containerBytes));
-
-            using var directoryScope = new ContainerDirectoryScope(_fileHandler.CreateContainerDirectory());
-            _fileHandler.ExtractContainerToDirectory(containerBytes, directoryScope.Directory);
-            var structurePath = Path.Combine(directoryScope.Directory, Constants.L3dXmlFilename);
-            var luminaire = _l3dXmlReader.Read(structurePath, directoryScope.Directory);
-            return _apiDtoConverter.Convert(luminaire, directoryScope.Directory);
-        }
+    private LuminaireDto ReadInternal(Action<string> extractAction)
+    {
+        using var directoryScope = new ContainerDirectoryScope(_fileHandler.CreateContainerDirectory());
+        extractAction(directoryScope.Directory);
+        var structurePath = Path.Combine(directoryScope.Directory, Constants.L3dXmlFilename);
+        var luminaire = _l3dXmlReader.Read(structurePath, directoryScope.Directory);
+        return _apiDtoConverter.Convert(luminaire, directoryScope.Directory);
     }
 }
--- src/L3D.Net/Reader.cs
+++ src/L3D.Net/Reader.cs
@@ -5,27 +5,21 @@ using L3D.Net.Internal;
 using L3D.Net.XML;
 using Microsoft.Extensions.Logging;
 
-namespace L3D.Net
+namespace L3D.Net;
+
+public class Reader : IReader
 {
-    public class Reader : IReader
+    public LuminaireDto ReadContainer(string containerPath, ILogger logger = null) =>
+        CreateContainerReader(logger).Read(containerPath);
+
+    public LuminaireDto ReadContainer(byte[] containerBytes, ILogger logger = null) =>
+        CreateContainerReader(logger).Read(containerBytes);
+
+    private static ContainerReader CreateContainerReader(ILogger logger)
     {
-        public LuminaireDto ReadContainer(string containerPath, ILogger logger = null)
-        {
-            return CreateContainerReader(logger).Read(containerPath);
-        }
-        
-        public LuminaireDto ReadContainer(byte[] containerBytes, ILogger logger = null)
-        {
-            return CreateContainerReader(logger).Read(containerBytes);
-        }
-
-        private ContainerReader CreateContainerReader(ILogger logger)
-        {
-            var fileHandler = new FileHandler();
-
-            return new ContainerReader(fileHandler,
-                                       new L3dXmlReader(new XmlValidator(), logger),
-                                       new ApiDtoConverter(fileHandler));
-        }
+        var fileHandler = new FileHandler();
+        return new ContainerReader(fileHandler,
+            new L3dXmlReader(new XmlValidator(), logger),
+            new ApiDtoConverter(fileHandler));
     }
 }
